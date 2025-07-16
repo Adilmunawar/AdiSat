35f821e77abae7f120a56af07a600c8ff0a6b412
Adil Munawar <127094634+Adilmunawar@users.noreply.github.com>
2025-07-16 13:45:32 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Or whatever CSS file it imports
-import App from './App'; // This should point to your AdiSatTrackerApp.js content
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // Import your main App component
+
+// You can add a basic CSS file if you want, e.g., src/index.css
+// import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +15,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// (You don't strictly need reportWebVitals for basic deployment, but it's part of CRA boilerplate)
+// import reportWebVitals from './reportWebVitals';
+// reportWebVitals();
